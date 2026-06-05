0ca84e28f97fae0b9e48a6769a8f576386c0512c
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:37 -0500

    xml commenting

--- src/Soenneker.Quo.HttpClients/QuoOpenApiHttpClient.cs
+++ src/Soenneker.Quo.HttpClients/QuoOpenApiHttpClient.cs
@@ -42,11 +42,18 @@ public sealed class QuoOpenApiHttpClient : IQuoOpenApiHttpClient
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(QuoOpenApiHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(QuoOpenApiHttpClient));
